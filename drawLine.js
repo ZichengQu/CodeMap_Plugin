18d8e61282cc7ec5a985889cff0385eaa1d13efe
ZichengQu <quzicheng315@gmail.com>
2019-10-29 14:32:15 +1100

    Read from test.json and drawLine as required.

--- drawLine.js
+++ drawLine.js
@@ -12,14 +12,12 @@ fs.readFile('/home/zichengqu/Desktop/CodeMapPlugin/test.json','utf8',function (e
   if(err) console.log(err);
   pointObj = eval("("+data+")");//convert json to js object
 
-  //console.log(pointObj.DrawLines);//输出从json转换成obj的对象
-
   for(var i=0; i<pointObj.DrawLines.length; i++){
     var key = pointObj.DrawLines[i].FilePath;//Key.
     var points = pointObj.DrawLines[i].Lines;//Points in Lines.
     if(pointMap.has(key)){//if the key is same, original value should plus new value.
       var oriPoints = pointMap.get(key);
-      var pointSplit = [{row: -1, colum: -1}];//use (-1,-1) as the separator between the different lines
+      var pointSplit = [{row: -1, column: -1}];//use (-1,-1) as the separator between the different lines in the same file
       var totalPoints = (oriPoints.concat(pointSplit)).concat(points);
       pointMap.set(key,totalPoints);
     }else{
@@ -27,32 +25,36 @@ fs.readFile('/home/zichengqu/Desktop/CodeMapPlugin/test.json','utf8',function (e
     }
   }
 
-  //console.log(pointMap);
   for (var [key, value] of pointMap) {
-    //console.log(key + ' = ' + value[0].row);//输出测试
+    //console.log(key + ' = ' + value[0].row);//console log test
     drawLine(key,value);
   }
 });
 
 var flag = true;
+
 function drawLine(key, value){
   let filepath = path.join(key)//Path of the C or C++ file
   let input = fs.createReadStream(filepath)
 
   var content = "";//The content of the C or C++ file
   var lineNumber = 1;//Count numbers for lines of the content
-
   var rl = readline.createInterface({
     input: input
   });
   rl.on('line', (line) => {
-      content += lineNumber++ +"    "+ line+ '\n';// Add line number for the C or C++ code.
+    var space = "";
+      if(lineNumber<=9){
+        space = "    ";
+      }else{
+        space = "   ";
+      }
+      content += lineNumber++ +space+ line+ '\n';// Add line number for the C or C++ code.
   });
   rl.on('close', (line) => {
-    
     var totalPoints = [{content:content}].concat(value);//Add the target file content into the value.
     pointMap.set(key,totalPoints);
-    //console.log(pointMap.get(key)[0].content);//测试当前循环下的test1或test2的content
+    //console.log(pointMap.get(key)[0].content);//Test the forEach content 
     
     app.use('/',express.static(path.join(__dirname,'')));
     //Allows parsing of URL-encoded forms; The extended option is set to false because there is no need to deal with any complicated objects.
